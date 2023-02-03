466873f0b93c20412a61a9585c32247abc621b30
Brody14 <stefania.spinas@gmail.com>
2023-02-03 16:14:28 +0100

    FEAT game

--- js/script.js
+++ js/script.js
@@ -2,28 +2,57 @@
 
 // CREARE UN ARRAY CON E-MAIL
 
-const mailList = [
-	"rfernandez@gmail.com",
-	"v.micov@outlook.com",
-	"avramovic.a@gmail.com",
-	"zobradovic@thecoach.com",
-];
+let choise = prompt(
+	"Scrivi mail se vuoi sapere se sei registrato o play se vuoi giocare con me!"
+);
 
-const printResult = document.getElementById("result");
-//console.log(mailList);
-// CHIEDERE A UTENTE SUA MAIL CON PROMPT
+if (choise === "mail") {
+	const mailList = [
+		"rfernandez@gmail.com",
+		"v.micov@outlook.com",
+		"avramovic.a@gmail.com",
+		"zobradovic@thecoach.com",
+	];
 
-const userMail = prompt("Inserisci il tuo indirizzo mail");
-//console.log(userMail);
+	const printResult = document.getElementById("result");
+	// //console.log(mailList);
+	// // CHIEDERE A UTENTE SUA MAIL CON PROMPT
 
-// CONTROLLARE SE LA MAIL E' PRESENTE NEL MIO ARRAY
+	const userMail = prompt("Inserisci il tuo indirizzo mail");
+	// //console.log(userMail);
 
-let message = "Non sei registrato";
+	// // CONTROLLARE SE LA MAIL E' PRESENTE NEL MIO ARRAY
 
-if (userMail >= mailList) {
-	message = "Sei già dei nostri!";
+	let message = "Non sei registrato";
+
+	if (userMail >= mailList) {
+		message = "Sei già dei nostri!";
+	}
+
+	// // STAMPARE ADEGUATO RISULTATO
+
+	printResult.innerHTML = message;
 }
 
-// STAMPARE ADEGUATO RISULTATO
+if (choise === "play") {
+	// GENERARE NUMERO RANDOM TRA 1 E 6 PER UTENTE E UNO PER PC
+	// dichiarare due variabili con numeri generati random, uno per uno
+	let randomUtente = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+	let randomPc = Math.floor(Math.random() * (6 - 1 + 1) + 1);
 
-printResult.innerHTML = message;
+	console.log(randomUtente, randomPc);
+
+	// STABILIRE IL VINCITORE
+	// confrontare quali tra i due è più altro
+	let gameMessage = "Hai perso!";
+
+	if (randomUtente > randomPc) {
+		gameMessage = "Hai vinto!";
+	}
+
+	console.log(gameMessage);
+
+	// stampare se ha vinto o meno
+
+	alert(gameMessage);
+}
