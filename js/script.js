08266bcec5554bd92d14697cd0dca8ad54e94167
Brody14 <stefania.spinas@gmail.com>
2023-02-03 14:27:06 +0100

    FEAT email array

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log("Tavarez");
+
+// CREARE UN ARRAY CON E-MAIL
+
+const mailList = [
+	"rfernandez@gmail.com",
+	"v.micov@outlook.com",
+	"avramovic.a@gmail.com",
+	"zobradovic@thecoach.com",
+];
+console.log(mailList);
+// CHIEDERE A UTENTE SUA MAIL CON PROMPT
+
+// CONTROLLARE SE LA MAIL E' PRESENTE NEL MIO ARRAY
+
+// STAMPARE ADEGUATO RISULTATO
