0f5aa25825c50e2e0b4cf9f67a9481d9504c66ff
Brody14 <stefania.spinas@gmail.com>
2023-02-05 21:04:56 +0100

    EDIT mail check + STYLE

--- js/script.js
+++ js/script.js
@@ -1,61 +1,76 @@
 //console.log("Tavarez");
 
 // CREARE UN ARRAY CON E-MAIL
+let stampContainer = document.getElementById('box');
+const btnElement = document.querySelector('.btn');
 
-let choise = prompt(
-	"Scrivi mail se vuoi sapere se sei registrato o play se vuoi giocare con me!"
-);
 
-if (choise === "mail") {
-	const mailList = [
-		"rfernandez@gmail.com",
-		"v.micov@outlook.com",
-		"avramovic.a@gmail.com",
-		"zobradovic@thecoach.com",
-	];
 
-	const printResult = document.getElementById("result");
-	// //console.log(mailList);
-	// // CHIEDERE A UTENTE SUA MAIL CON PROMPT
+const mailList = [
+	"rfernandez@gmail.com",
+	"v.micov@outlook.com",
+	"avramovic.a@gmail.com",
+	"zobradovic@thecoach.com",
+];
 
-	const userMail = prompt("Inserisci il tuo indirizzo mail");
-	// //console.log(userMail);
+// //console.log(mailList);
 
-	// // CONTROLLARE SE LA MAIL E' PRESENTE NEL MIO ARRAY
+// // CONTROLLARE SE LA MAIL E' PRESENTE NEL MIO ARRAY
 
-	let message = "Non sei registrato";
+btnElement.addEventListener('click', function (){
+	console.log('click')
+	let choise = document.getElementById("e-mail");
+	let mail = choise.value;
+	let message = '';
+	const addBtnElement = document.querySelector('.d-none');
+
+	message = "Non sei registrato, ma puoi rimediare!"
+	
+	addBtnElement.classList.toggle('d-none')
+
+	addBtnElement.addEventListener('click', function () {
+		mailList.push(mail);
+		console.log(mailList)
+		addBtnElement.classList.toggle('d-none')
+		stampContainer.innerHTML = 'Ottimo! Ti sono stati addebitati 10.000€!'
+	})
+	
 
 	for (let i = 0; i < mailList.length; i++) {
 		//console.log(i, mailList[i]);
 
-		if (userMail === mailList[i]) {
+		if (mail === mailList[i]) {
 			message = "Sei già dei nostri!";
-		}
+			addBtnElement.classList.toggle('d-none')
+		} 
 	}
-	// // STAMPARE ADEGUATO RISULTATO
+	
+	stampContainer.innerHTML = message
+	
+})
 
-	printResult.innerHTML = message;
-}
 
-if (choise === "play") {
-	// GENERARE NUMERO RANDOM TRA 1 E 6 PER UTENTE E UNO PER PC
-	// dichiarare due variabili con numeri generati random, uno per uno
-	let randomUtente = Math.floor(Math.random() * (6 - 1 + 1) + 1);
-	let randomPc = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+// // // STAMPARE ADEGUATO RISULTATO
 
-	console.log(randomUtente, randomPc);
+// printResult.innerHTML = message;
 
-	// STABILIRE IL VINCITORE
-	// confrontare quali tra i due è più altro
-	let gameMessage = "Hai perso!";
+// // GENERARE NUMERO RANDOM TRA 1 E 6 PER UTENTE E UNO PER PC
+// // dichiarare due variabili con numeri generati random, uno per uno
+// let randomUtente = Math.floor(Math.random() * 6 + 1);
+// let randomPc = Math.floor(Math.random() * 6 + 1);
 
-	if (randomUtente > randomPc) {
-		gameMessage = "Hai vinto!";
-	}
+// console.log(randomUtente, randomPc);
 
-	console.log(gameMessage);
+// // STABILIRE IL VINCITORE
+// // confrontare quali tra i due è più altro
+// let gameMessage = "Hai perso!";
 
-	// stampare se ha vinto o meno
+// if (randomUtente > randomPc) {
+// 	gameMessage = "Hai vinto!";
+// }
 
-	alert(gameMessage);
-}
+// console.log(gameMessage);
+
+// // stampare se ha vinto o meno
+
+// alert(gameMessage);
